da8bd7139c6bed989ba29d312823b31b8921f72d
Williammer <supervegeter@gmail.com>
2015-07-07 23:29:56 +0800

    added BoardSpec

---
+++ test/BoardSpec.js
@@ -0,0 +1,23 @@
+// [O] Board module
+describe("#Board test", function() {
+    beforeEach(function() {
+
+    });
+
+    it("should have 16 cards", function() {
+
+    });
+    // [improve] can have changeable scales of cards selected by user.
+
+    it("should shuffle cards on each start", function() {
+
+    });
+
+    it("should trigger a card match process once 2 cards are flipped", function() {
+
+    });
+
+    it("should win the game when all cards are matched", function() {
+
+    });
+});
